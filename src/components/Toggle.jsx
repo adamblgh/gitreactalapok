b312c4afb23e3f3a378b3ec75b618a39e05381bd
Your Name <you@example.com>
2022-09-16 11:29:03 +0200

    todo

--- src/components/Toggle.jsx
+++ src/components/Toggle.jsx
@@ -5,7 +5,7 @@ import { Button } from 'react-bootstrap';
 export const Toggle=()=> {
   const [isHappy,setisHappy]=useState(true)
   return (
-    <div>
+    <div className='text-center mb-5'>
       <Button onClick={()=>setisHappy(!isHappy)}>Are you happy? {isHappy?'😀':'😒'}</Button>
     </div>
   )
